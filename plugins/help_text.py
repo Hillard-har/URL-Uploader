7d0604873d6ccbacb1142bd33010481cd605140b
Hillard-har <73195851+Hillard-har@users.noreply.github.com>
2020-11-01 00:26:36 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
