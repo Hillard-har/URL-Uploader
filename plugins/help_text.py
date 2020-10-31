741f1357667bae7509c4d602487659cba5fd20b3
Hillard-har <73195851+Hillard-har@users.noreply.github.com>
2020-11-01 00:34:15 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -35,7 +35,7 @@ def GetExpiryDate(chat_id):
 @pyrogram.Client.on_callback_query()
 async def btn_handler(bot, update):
 
-if 'help' in update.data:
+      if 'help' in update.data:
           await update.message.delete()
           await help_user(bot, update.message)
 
